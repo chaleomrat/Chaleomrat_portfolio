6a0c41fe249dd26df4368cb8209641029c4b3613
thanakit <thanakittinop05@hotmail.com>
2020-09-28 10:28:31 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,24 @@
+const data ={
+    "name":"Chaleomrat Phromsaart",
+    "work":"Student",
+    "aboutme":{
+        "Fullname":"Chaleomrat Phromsaart",
+        "Phone":"(+66)91 004 4851",
+        "Email":"fxcz.777@gmail.com",
+        "Website":"https://asdasd.com",
+        "Address":"232/4 Phang Phuai Subdistrict, Damnoen Saduak District ,Ratchaburi",
+
+    }
+   ,
+   
+    "scripts": {
+        "start": "node index.js"
+      },
+     "engines": {
+        "node": "v12.18.0",
+        "npm": "6.14.4"
+      }
+    
+};
+
+module.exports =data;
