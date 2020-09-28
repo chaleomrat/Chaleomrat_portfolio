fcd53c748abaf7fe2ee03b495a4cc8178a4e236c
thanakit <thanakittinop05@hotmail.com>
2020-09-28 10:43:29 +0700

    second commit

--- data.js
+++ data.js
@@ -8,17 +8,7 @@ const data ={
         "Website":"https://asdasd.com",
         "Address":"232/4 Phang Phuai Subdistrict, Damnoen Saduak District ,Ratchaburi",
 
-    }
-   ,
+    }};
    
-    "scripts": {
-        "start": "node index.js"
-      },
-     "engines": {
-        "node": "v12.18.0",
-        "npm": "6.14.4"
-      }
-    
-};
 
 module.exports =data;
